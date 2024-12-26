ec637c0ed1d5a9ab181cb010ab681347937eb525
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-12-26 17:07:07 +0000

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Display from '../components/Display'
+// import '../styles/App.css'
 
 const App = () => {
     return(
       <main id="main">
+        <Display />
       </main>
     )
 }
---
+++ src/components/Display.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import items from "../components/items";
+
+function Display() {
+  let [card, setCard] = useState(items);
+  let [reload, setReload] = useState(false);
+  let [isEmpty, setEmpty] = useState(false);
+  let [showmore, setShowmore] = useState([]);
+
+  function handleShowmore(index) {
+    const updatedShowmore = [...showmore];
+    updatedShowmore[index] = !showmore[index];
+    setShowmore(updatedShowmore);
+  }
+
+  function handleDelete(index) {
+    const updated = card.filter((item, i) => i !== index);
+    setCard(updated);
+
+    if (card.length <= 1) {
+      setEmpty(true);
+    }
+  }
+
+  function handleRefresh() {
+    if (card.length === 0 && !reload) {
+      window.location.reload();
+    }
+    setReload(true);
+  }
+
+  return (
+    <div className="tour-list">
+      <h1 className="title">Tours</h1>
+      <button onClick={handleRefresh} className="btn">
+        Refresh
+      </button>
+      {reload ? (
+        <p>Loading...</p>
+      ) : !isEmpty ? (
+        card.map((item, index) => {
+          return (
+            <div id="tour-item-para-rec6d6T3q5EBIdCfD" key={index} className="single-tour">
+              <img src={item.image} alt={item.name} />
+              <p>{item.name}</p>
+              <p className="tour-info">{showmore[index] ? null : item.info}</p>
+              <p className="tour-price">${item.price}</p>
+              <button id="delete-btn-rec6d6T3q5EBIdCfD" onClick={() => handleDelete(index)} className="delete-btn">
+                Delete
+              </button>
+              <button
+                onClick={() => handleShowmore(index)}
+                className="show-more"
+              >
+                {showmore[index] ? "See more" : "Show less"}
+              </button>
+            </div>
+          );
+        })
+      ) : (
+        <p>No more tours</p>
+      )}
+    </div>
+  );
+}
+
+export default Display;
